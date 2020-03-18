087c6b73a1de42c0405e48b94d4e8884407e2a08
mferreira95 <migasferreira@gmail.com>
2020-03-18 12:07:24 +0000

    first commit

---
+++ src/antproj2/AntProj2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antproj2;
+
+/**
+ *
+ * @author Miguel Ferreira
+ */
+public class AntProj2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world from antproj2!!!");
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/antproj2/Mensagem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package antproj2;
+
+/**
+ *
+ * @author Miguel Ferreira
+ */
+public class Mensagem {
+    
+    private final String m;
+    
+    public Mensagem(){
+        this.m = "Hello World from class Mensagem antproj2";
+    }
+    
+    public String getMessage(){
+        return m;
+    }
+}
